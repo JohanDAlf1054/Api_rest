04fae93d60154f3ae4ce0e4380800f22384ced99
Johan Alfonso <jdaalfonso@gmail.com>
2023-09-27 07:42:45 -0500

    Comentarios en el archivo app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const express = require('express');
+//Se inicia el modulo expres para dar inicio al servidor, evitando configuraciones.
+const express = require('express'); //Se importa el paquete
 const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser')
@@ -8,11 +9,14 @@ app.use(bodyParser.json());
 const postRoute = require('./routes/post');
 app.use('/servicios',postRoute);
 
+//En el siguiente bloque de codigo se crean las rutas.
+
 app.get('/', (req, res)=>{
     res.send('Prueba 1 respuesta del servidor');
 });
 
 async function connectToDatabase() {
+    //Prueba de conecxion hacia la BD.
     try {
         await mongoose.connect('mongodb+srv://Johan_Al:Johanalfonso1054@cluster0.3eby90e.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp', {
             useNewUrlParser: true,
@@ -24,7 +28,7 @@ async function connectToDatabase() {
         console.error('Error connecting to MongoDB:', error);
     }
 }
-
+//Conecxion de la BD
 connectToDatabase();
 
 
